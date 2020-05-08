34434ff30e8e3f1dd7ce1a1c2e6fed1a3fff0563
unknown <precie2010.pd@gmail.com>
2020-05-08 02:11:30 -0600

    first commit

---
+++ tests/booksintegrationTests.js
@@ -0,0 +1,37 @@
+const should = require('should');
+
+const request = require('supertest');
+const mongoose = require('mongoose');
+
+const app = require('../app.js');
+
+const Book = mongoose.model('Book');
+const agent = request.agent(app);
+
+process.env.ENV = 'Test';
+
+describe('Book Crud Test', () =>{
+  it('should allow a book to be posted and read and _it',(done) =>{
+    const bookPost = {title: 'My Book', author: 'Jon', genre:'Fiction'};
+
+    agent.post('/api/books')
+      .send(bookPost)
+      .expect(200)
+      .end((err,results) => {
+        //console.log(results);
+        //results.body.read.should.not.equal('false');
+        results.body.should.have.property('_id');
+        done();
+      });
+  });
+
+  afterEach((done) => {
+    Book.deleteMany({}).exec();
+    done();
+  });
+
+  after((done)=>{
+    mongoose.connection.close();
+    app.server.close(done());
+  });
+});
